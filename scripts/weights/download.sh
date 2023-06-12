d248b3a1c9ab699458f295fc3136da4650b9425e
JinhaSong <jinhasong@sogang.ac.kr>
2023-06-12 20:32:01 +0900

    Update model download script

--- scripts/weights/download.sh
+++ scripts/weights/download.sh
@@ -10,7 +10,13 @@ else
 fi
 
 echo "======================================="
-echo "Download start(yolov5m-face)"
-wget ftp://mldisk.sogang.ac.kr/models/yolov5-face/yolov5m-face.pt -O /workspace/models/weights/yolov5m-face.pt
-echo "Download successfully complete(yolov5m-face)"
+echo "Download start(yolov5-face)"
+wget -q ftp://mldisk.sogang.ac.kr/models/yolov5-face/yolov5m-face.pt -O /workspace/models/weights/yolov5m-face.pt \
+&& echo "Download successful(yolov5-face)" \
+|| echo -e "\e[31mDownload faile(yolov5-face)\e[0m"
+echo "======================================="
+echo "Download start(yolov7-face)"
+wget -q ftp://mldisk.sogang.ac.kr/models/yolov7-face/yolov7-w6-face.pt -O /workspace/models/weights/yolov7-w6-face.pt \
+&& echo "Download successful(yolov7-face)" \
+|| echo -e "\e[31mDownload faile(yolov7-face)\e[0m"
 echo "======================================="
