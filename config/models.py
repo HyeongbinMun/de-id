5a1766df04da937b5c8fe0712c2a90444bfa81ab
JinhaSong <jinhasong@sogang.ac.kr>
2023-06-13 19:51:56 +0900

    Update submodules

--- config/models.py
+++ config/models.py
@@ -1,9 +1,10 @@
-
+from model.det.face.yolov5.yolov5_face import YOLOv5Face
+from model.det.face.yolov7.yolov7_face import YOLOv7Face
 
 models = {
     "det": {
-        "yolov5-face": "",
-        "yolov5-face": "",
+        "yolov5-face": YOLOv5Face,
+        "yolov7-face": YOLOv7Face,
     },
     "deid": {
         "cyclegan": "",
