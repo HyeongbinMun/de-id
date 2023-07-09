5ae2dea423f5e20e7c220b47d8bdbd38c5a2b26f
JinhaSong <jinhasong@sogang.ac.kr>
2023-07-09 15:37:49 +0900

    Update model class dictionary

--- config/models.py
+++ config/models.py
@@ -1,13 +1,26 @@
 from model.det.face.yolov5.yolov5_face import YOLOv5Face
 from model.det.face.yolov7.yolov7_face import YOLOv7Face
+from model.vcd.models.mobilenetv2_avg import MobileNet_AVG
+from model.deid.feature_inversion.models.resnet50_unet import ResNet50UNetInverter
+from model.deid.feature_inversion.models.mobilenetv2_unet import MobileNetV2UNetInverter
+from model.deid.feature_inversion.models.mobilenetv3_unet import MobileNetV3SmallUNetInverter
+from model.deid.feature_inversion.models.mobilenetv3_unet import MobileNetV3LargeUNetInverter
 
-models = {
+model_classes = {
     "det": {
-        "yolov5-face": YOLOv5Face,
-        "yolov7-face": YOLOv7Face,
+        "face": {
+            "yolov5": YOLOv5Face,
+            "yolov7": YOLOv7Face,
+        }
+    },
+    "feature": {
+        "MobileNet_AVG": MobileNet_AVG
     },
     "deid": {
         "cyclegan": "",
-        "feature_inversion": ""
-    }
+        "ResNet50UNetInverter": ResNet50UNetInverter,
+        "MobileNetV2UNetInverter": MobileNetV2UNetInverter,
+        "MobileNetV3SmallUNetInverter": MobileNetV3SmallUNetInverter,
+        "MobileNetV3LargeUNetInverter": MobileNetV3LargeUNetInverter,
+    },
 }
