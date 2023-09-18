94d599360ccabb8483d8fade9552b4948d1f15d9
JinhaSong <jinhasong@sogang.ac.kr>
2023-09-18 17:39:27 +0900

    Update preprocessing script (remove_no_object_image.py) - fixed bug

--- tools/preprocess/common/remove_no_object_image.py
+++ tools/preprocess/common/remove_no_object_image.py
@@ -3,22 +3,24 @@ from tqdm import tqdm
 import argparse
 
 def remove_missing_labels(images_dir, labels_dir):
-    images_files = sorted(os.listdir(images_dir))
-    labels_files = sorted(os.listdir(labels_dir))
+    image_files = [os.path.splitext(f)[0] for f in os.listdir(images_dir) if f.endswith('.jpg')]
+    label_files = [os.path.splitext(f)[0] for f in os.listdir(labels_dir) if f.endswith('.txt')]
 
-    labels_images = set([file.split(".")[0] for file in labels_files])
+    # 집합 변환 (빠른 검색을 위해)
+    image_files_set = set(image_files)
+    label_files_set = set(label_files)
 
-    for image_file in tqdm(images_files, desc="Resizing images"):
-        image_name = image_file.split(".")[0]
-        if image_name not in labels_images:
-            image_path = os.path.join(images_dir, image_file)
-            os.remove(image_path)
-            print(f"Deleted image: {image_file}")
+    images_without_labels = image_files_set - label_files_set
+
+    for image_name in tqdm(images_without_labels, desc="Deleting images"):
+        os.remove(os.path.join(images_dir, image_name + '.jpg'))
+        print(f"Deleted image: {os.path.join(images_dir, image_name + '.jpg')}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Remove images without corresponding labels")
-    parser.add_argument("--images_dir", type=str, default="/dataset/images_1080_0.1/images", help="images directory")
-    parser.add_argument("--labels_dir", type=str, default="/dataset/images_1080_0.1/labels", help="labels directory")
+    parser.add_argument("--images_dir", type=str, default="/dataset/dnadb/images", help="images directory")
+    parser.add_argument("--labels_dir", type=str, default="/dataset/dnadb/labels", help="labels directory")
 
     args = parser.parse_args()
 
