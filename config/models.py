9c2a5dfb270844fb0180666a7a7f0a6efc20fa6a
JinhaSong <jinhasong@sogang.ac.kr>
2023-10-04 03:48:44 +0900

    Update feature inversion models - mobilenetv2, resnet50 기반 feature inversion 모델 추가

--- config/models.py
+++ config/models.py
@@ -2,6 +2,8 @@ from model.det.face.yolov5.yolov5_face import YOLOv5Face
 from model.det.face.yolov7.yolov7_face import YOLOv7Face
 from model.vcd.models.mobilenetv2_avg import MobileNet_AVG
 from model.icd.sscd.sscd.models.model import Model as ResNet
+from model.deid.feature_inversion.models.mobilenetv2_inversion import MobileNetV2Inverter
+from model.deid.feature_inversion.models.resnet50_inversion import ResNet50Inverter
 from model.deid.feature_inversion.models.mobile_unet import MobileUNetInverter
 from model.deid.feature_inversion.models.resnet50_unet import ResNet50UNetInverter
 from model.deid.feature_inversion.models.mobilenetv2_unet import MobileNetV2UNetInverter
@@ -26,5 +28,7 @@ model_classes = {
         "MobileNetV2UNetInverter": MobileNetV2UNetInverter,
         "MobileNetV3SmallUNetInverter": MobileNetV3SmallUNetInverter,
         "MobileNetV3LargeUNetInverter": MobileNetV3LargeUNetInverter,
+        "MobileNetV2Inverter": MobileNetV2Inverter,
+        "ResNet50Inverter": ResNet50Inverter,
     },
 }
